d1ff8981d7c276a29c43a2ee1b307538e701ba85
wander4747 <wander.douglas14@gmail.com>
2024-12-19 18:20:35 -0300

    variables and mutability

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,8 @@ FROM rust:alpine
 
 RUN apk add --no-cache musl-dev openssl-dev
 
-RUN cargo install cargo-watch
+RUN cargo install
 
 WORKDIR /usr/src/app
 
 COPY . .
-
-CMD ["cargo-watch", "-x", "run"]
---
+++ src/bin/1-variable.rs
@@ -0,0 +1,12 @@
+const SECONDS: i32 = 60;
+
+fn main() {
+    println!("Inicio do programa");
+
+    let mut x: i32 = 5;
+
+    println!("O valor de x é: {x}");
+
+    x = SECONDS;
+    println!("O valor de x agora é: {x}");
+}
